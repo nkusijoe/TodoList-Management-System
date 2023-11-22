322e2918512a57c4d4f35a22897b6f54c062c48e
nkusijoe <joenkusi01@gmail.com>
2023-11-22 18:47:19 +0200

    created task routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const taskController = require('../controllers/taskController');
+
+const router = express.Router();
+
+// CRUD routes for tasks
+router.post('/', taskController.createTask);
+router.get('/', taskController.getAllTasks);
+router.get('/:id', taskController.getTaskById);
+router.put('/:id', taskController.updateTaskById);
+router.delete('/:id', taskController.deleteTaskById);
+
+module.exports = router;
